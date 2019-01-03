db766bb19c2c227829504fcbe8f57076407b1f0a
Eli Cleveland <elicleveland12@gmail.com>
2019-01-03 03:29:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
         }
      }
 
-    programmer_hash = {:dennis_ritchie => {:known_for => "Unix", :languages => ["C", "Assembly"]
+    programmer_hash = {:dennis_ritchie => {:known_for => "Unix", :languages => ["C", "Assembly"]}}
     programmer_hash[:dennis_ritchie][:languages]
 end
